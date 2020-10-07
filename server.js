39aaf3557dc9075c86ec5b80f1133bf9c8b5320f
itsmanojb <the_manoj@live.com>
2020-10-08 01:39:11 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const ejs = require('ejs');
+const expressLayouts = require('express-ejs-layouts');
+const path = require('path');
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('home');
+});
+
+app.use(expressLayouts);
+app.set('views', path.join(__dirname, 'resources/views'));
+app.set('view engine', 'ejs');
+
+app.listen(PORT, () => {
+  console.log(`Listening to port ${PORT}`);
+});
