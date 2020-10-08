8d227f445e3072baa83fccbd147245d023ea1e7c
itsmanojb <the_manoj@live.com>
2020-10-09 00:24:37 +0530

    more routes added

--- public/js/app.js
+++ public/js/app.js
@@ -115,8 +115,8 @@
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/innostratconsulting/Work/node/domonos/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/innostratconsulting/Work/node/domonos/resources/scss/app.scss */"./resources/scss/app.scss");
+__webpack_require__(/*! /home/moriarty/Work/Node/domonos/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/moriarty/Work/Node/domonos/resources/scss/app.scss */"./resources/scss/app.scss");
 
 
 /***/ })
--- server.js
+++ server.js
@@ -6,13 +6,26 @@ const path = require('path');
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
-app.get('/', (req, res) => {
-  res.render('home');
-});
 
 app.use(expressLayouts);
 app.set('views', path.join(__dirname, 'resources/views'));
 app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.get('/cart', (req, res) => {
+  res.render('customers/cart');
+});
+
+app.get('/login', (req, res) => {
+  res.render('auth/login');
+});
+
+app.get('/register', (req, res) => {
+  res.render('auth/register');
+});
 
 app.listen(PORT, () => {
   console.log(`Listening to port ${PORT}`);
