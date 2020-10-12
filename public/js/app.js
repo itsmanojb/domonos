0c1293a1646d1e10a264918c16464cd730fc5f70
itsmanojb <the_manoj@live.com>
2020-10-12 21:59:44 +0530

    menu-header added

--- public/js/app.js
+++ public/js/app.js
@@ -5262,6 +5262,18 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var axios__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(axios__WEBPACK_IMPORTED_MODULE_0__);
 /* harmony import */ var noty__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! noty */ "./node_modules/noty/lib/noty.js");
 /* harmony import */ var noty__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(noty__WEBPACK_IMPORTED_MODULE_1__);
+function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableSpread(); }
+
+function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _iterableToArray(iter) { if (typeof Symbol !== "undefined" && Symbol.iterator in Object(iter)) return Array.from(iter); }
+
+function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) return _arrayLikeToArray(arr); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
 
 
 var addtoCartBtn = document.querySelectorAll('.addToCart');
@@ -5292,6 +5304,38 @@ addtoCartBtn.forEach(function (btn) {
     updateCart(pizza);
   });
 });
+document.addEventListener('DOMContentLoaded', function () {
+  var sections = document.querySelectorAll(".scrollspy-section");
+  var menu_links = document.querySelectorAll(".scrollspy-link a");
+
+  var makeActive = function makeActive(link) {
+    return menu_links[link].classList.add("active");
+  };
+
+  var removeActive = function removeActive(link) {
+    return menu_links[link].classList.remove("active");
+  };
+
+  var removeAllActive = function removeAllActive() {
+    return _toConsumableArray(Array(sections.length).keys()).forEach(function (link) {
+      return removeActive(link);
+    });
+  };
+
+  var sectionMargin = 200;
+  var currentActive = 0;
+  window.addEventListener("scroll", function () {
+    var current = sections.length - _toConsumableArray(sections).reverse().findIndex(function (section) {
+      return window.scrollY >= section.offsetTop - sectionMargin;
+    }) - 1;
+
+    if (current !== currentActive) {
+      removeAllActive();
+      currentActive = current;
+      makeActive(current);
+    }
+  });
+}, false);
 
 /***/ }),
 
--- resources/js/app.js
+++ resources/js/app.js
@@ -28,4 +28,27 @@ addtoCartBtn.forEach(btn => {
     const pizza = JSON.parse(btn.dataset.pizza);
     updateCart(pizza);
   })
-})
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+
+  const sections = document.querySelectorAll(".scrollspy-section");
+  const menu_links = document.querySelectorAll(".scrollspy-link a");
+
+  const makeActive = (link) => menu_links[link].classList.add("active");
+  const removeActive = (link) => menu_links[link].classList.remove("active");
+  const removeAllActive = () => [...Array(sections.length).keys()].forEach((link) => removeActive(link));
+
+  const sectionMargin = 200;
+  let currentActive = 0;
+
+  window.addEventListener("scroll", () => {
+
+    const current = sections.length - [...sections].reverse().findIndex((section) => window.scrollY >= section.offsetTop - sectionMargin) - 1
+    if (current !== currentActive) {
+      removeAllActive();
+      currentActive = current;
+      makeActive(current);
+    }
+  });
+}, false);
