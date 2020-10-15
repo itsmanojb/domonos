eca7930ae9437f9fc2b054927be7a31a540fd3e2
itsmanojb <the_manoj@live.com>
2020-10-15 23:37:17 +0530

    all menus - displayed on home

--- app/http/controllers/homeController.js
+++ app/http/controllers/homeController.js
@@ -3,9 +3,36 @@ const Menu = require('../../models/menu');
 function homeController() {
   return {
     async index(req, res) {
-      const pizzas = await Menu.find();
+      const allMenus = await Menu.find();
+
+      const menus = allMenus.reduce((menus, item) => {
+        const group = (menus[item.menuType] || []);
+        group.push(item);
+        menus[item.menuType] = group;
+        return menus;
+      }, {});
+
+      const veg = [],
+        nonveg = [];
+      const shuffled = menus.pizza.sort(() => 0.5 - Math.random());
+
+      menus.pizza.map(item => {
+        if (item.foodType === 'veg') {
+          veg.push(item);
+        } else {
+          nonveg.push(item);
+        }
+      });
+
+      // res.json(menus)
+
       res.render('home', {
-        pizzas
+        menus: {
+          ...menus,
+          veg,
+          nonveg,
+          topMenu: shuffled.slice(0, 12)
+        }
       });
     },
   };
